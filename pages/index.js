c9bd16d5ec08f98e71d69b58d5e3c6c6d21f06c6
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2022-12-28 11:13:21 +0530

    api-working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
-
+import styles from '../styles/Home.module.css'
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
@@ -18,9 +18,9 @@ function App() {
 
   return (
     //  main body 
-    <div className="app">
+    <div className={styles.app}>
       {/* location input  */}
-      <div className="search_bar">
+      <div className={styles.search_bar}>
         <input
           value={location}
           placeholder='Enter your Location!!'
@@ -31,31 +31,31 @@ function App() {
       </div>
 
       {/* weather information  */}
-      <div className="container">
-        <div className="top_box">
-          <div className="location">
+      <div className={styles.container}>
+        <div className={styles.top_box}>
+          <div className={styles.location}>
             <p>{data.name}</p>
           </div>
-          <div className="temp">
+          <div className={styles.temp}>
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="description">
+          <div className={styles.description}>
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {/* extra information  */}
         {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
+          <div className={styles.bottom}>
+            <div className={styles.feels}>
               {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
               <p>Feels Like</p>
             </div>
-            <div className="wind">
+            <div className={styles.wind}>
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
               <p>Wind Speed</p>
             </div>
-            <div className="humidity">
+            <div className={styles.humidity}>
               {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
